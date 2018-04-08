7d85581226deae17a7b8f7f4d6bacbeef81c598e
Peter Polle <peter.m.polle@gmail.com>
2018-04-08 16:01:48 +0300

    created sources model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,country):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-   	NEWS_SOURCES_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     
 
