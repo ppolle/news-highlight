c9bfc5642d9f6182501cc44fa821500fc212cfbc
Peter Polle <peter.m.polle@gmail.com>
2018-04-08 15:51:32 +0300

    updated the config.py file to include news sources base url and api key environment variable

---
+++ .gitignore
@@ -0,0 +1,3 @@
+virtual/
+*.pyc
+start.sh
---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+class Config:
+
+   	NEWS_SOURCES_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
