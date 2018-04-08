2ad42b9b2e3e872f9786d7e5ee2ff72dd1bff026
Peter Polle <peter.m.polle@gmail.com>
2018-04-08 17:08:19 +0300

    created index.html and updated views.py to enable rendering of source names on the index page

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources
+from ..models import Sources
+
+#views
+@main.route('/')
+def index():
+	'''
+	view root page function that returns the index the page and its data
+	'''
+	get_sources = get_sources()
+	return render_template('index.html', get_sources = get_sources)
