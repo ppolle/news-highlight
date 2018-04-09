677fe237a795d7f0b95ad24151d7f70cfc63483e
Peter Polle <peter.m.polle@gmail.com>
2018-04-09 11:32:39 +0300

    re-worked index page display of news sources

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
    	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey={}'
-   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
    	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
    	@staticmethod
    	def init_app(app):
