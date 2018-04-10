c81808c775b0ac66646d7af90ce2f9a78d9afaa9
Peter Polle <peter.m.polle@gmail.com>
2018-04-10 17:12:34 +0300

    updated NEWS_SOURCE_BASE_URL to be able to take category as a parameter

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey={}'
+   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
    	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
    	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
    	@staticmethod
